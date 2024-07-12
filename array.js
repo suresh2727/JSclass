e0bfbef950c098474b5972f449c52a7c0e0bbfc4
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-07-12 22:57:12 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+//By using array literal
+
+// var arr=[100 ,"virat" , 30.9 , true , {name:"suresh" , city: "bangalore"} , false ]
+// console.log(arr[4].name);
+
+// var arr1 = new Array(1,2,"suresh" , true , false , 'A')
+// console.log(arr1);
+// console.log(arr1[2]);
+// //to find the length of property 
+// console.log(arr.length);
+
+// How to create a 2d array and access it 
+
+// let arr = [10, 20, true , [20,30,40] , "suresh" , "c" , 200]
+// console.log(arr.length);
+// console.log(arr[1]);
+// console.log(arr[3][2]);
+
+
+// how to create a 3d array 
+
+// let arr1 = [10, 20, "suresh" , [10, 30, ["suresh" , "mistry" , 5] , 40 ] , 60 ,70]
+// console.log(arr1[3][2][1]);
---
+++ arraymethods.js
@@ -0,0 +1,86 @@
+//push() method
+
+// var arr = [10]
+// arr.push(20)
+// arr.push(10)
+// arr.push("suresh")
+// arr.push("mistry")
+// console.log(arr);
+
+
+
+//POP method 
+
+// var arr1 = ['suresh' , 10 , 20 , 30 ,50 , "suresh"]
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+
+
+
+//unshift() method
+
+// var arr2= [10, "Sachin" , true , 27.90]
+// console.log(arr2);
+// arr2.unshift("suresh")
+// console.log(arr2);
+// arr2.unshift(18)
+// console.log(arr2);
+
+
+
+//shift() method 
+
+// var arr1 = ['suresh' , 34 , 44 , "mistry"]
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+
+
+//indexof method()
+
+// var arr1 = [10, "suresh" , true , 15.43 , "virat" , 'y']
+// console.log(arr1.indexOf(true));
+// console.log(arr1.indexOf("suresh"));
+// console.log(arr1.indexOf("bmw"));
+
+
+//lastindexOf
+//It is use to find the last repeating index number in the given array 
+
+// var arr5 = ["suresh" , "mistry" , 100 , true , 100 , 100, 100, 50 , 60 ]
+// console.log(arr5);
+// console.log(arr5.lastIndexOf(100));
+
+
+//Join methods 
+
+// var arr1 = [10, 'suresh' , true , 15.623 , "virat" , 'y']
+// console.log(arr1);
+// console.log(arr1.join());
+// console.log(arr1.join(' '));
+// console.log(arr1.join('-'));
+
+
+//Includes method()
+
+// var arr1 = [10, "Suresh" , "mistry" , 35 ,344 ,567]
+// console.log(arr1.includes("Suresh"));
+// console.log(arr1.includes("java"));
+
+
+//Reverse method
+
+// var arr1 = [10, "Suresh" , "mistry" , 35 ,344 ,567]
+// console.log(arr1);
+// console.log(arr1.reverse());
+
+
+//
+
+
+
+
+
