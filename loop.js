932cb83942d8a32f5a5804d30c9cccf1c9f918af
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-07-16 22:23:18 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,57 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     console.log("NUmber is ", index);
+    
+// }
+
+
+//while loop
+
+// var counter = 0;
+
+// while (counter<=10) {
+//     console.log("Counter value is " + counter);
+//     counter++;
+// }
+
+
+
+//do while loop
+
+// var count = 0
+// do{
+//     console.log("Count is " + count);
+//     count++;
+// }
+// while (count<0)
+
+
+
+//for in loop we can use only in obj loop
+
+// var obj = {
+//     name:"virat",
+//     age : 35,
+//     city: "Mumbai"
+// }
+
+// for (const key of obj) {
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+
+
+//for of loop wecan use in array
+//  var arr = [10, 40 ,55 , 56 , 45 ,34]
+// for (const iterator in arr) {
+//     console.log(arr[iterator]);
+// }
+
+
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
+    
+// }
