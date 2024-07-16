cdf1e59fdb7a52320143422a81b6b10594dcfe3f
suresh mistry <52049092+suresh2727@users.noreply.github.com>
2024-07-16 22:22:58 +0530

    Add files via upload

---
+++ control.js
@@ -0,0 +1,87 @@
+// if condition
+
+// function  FindEvenNumber(num) {
+//     if (num%2==0) {
+//         console.log("It is a even number");
+//     }
+// }
+// FindEvenNumber(3)
+
+//if else condition
+
+// function EvenorOdd(num) {
+//     if(num%2==0){
+//         console.log("It is a even Number");
+//     }
+//     else{
+//         console.log("It is a odd number");
+//     }
+// }
+
+// EvenorOdd(2)
+// EvenorOdd(7)
+
+//else if
+
+// function wish(message) {
+//     if (message === "gm") {
+//         console.log("Good Morning");
+//     }
+//     else if (message === "ga") {
+//         console.log("Good Afternoon");
+//     }
+//     else if (message === "ge") {
+//         console.log("Good Evening");
+//     }
+//     else if (message === "gn") {
+//         console.log("Good Night");
+//     }
+//     else{
+//         console.log("Your message is something else");
+//     }
+
+// }
+
+// wish("gn")
+// wish("Suresh")
+
+//switch
+
+// function wish(message) {
+//     switch (message) {
+//         case "gn":
+//             console.log("Good Night");
+//             break;
+
+//         case "gm":
+//             console.log("Good Morning");
+//             break;
+
+//         case "ge":
+//             console.log("Good Evening");
+//             break;
+
+//         case "ga":
+//             console.log("Good Afternoon");
+//             break;
+
+//         default: console.log("Your Messsage is invalid");
+//             break;
+//     }
+// }
+
+// wish("gn")
+// wish("gf")
+
+//Ternary operator
+
+// var a = 10;
+// var b = 20;
+
+// a > b ?console.log("True"): console.log("False");
+
+
+// var fname = "Suresh"
+// fname.includes("Mistry")?console.log("My name is mistry"): console.log("Your name is something else");
+
+
